f24d254e6363cc161e5a24f8bf78e3f93c709b79
halle <halle15@uw.edu>
2025-01-07 13:36:04 -0800

    shiny server

--- server.R
+++ server.R
@@ -5,19 +5,16 @@ library(plotly)
 library(ggplot2)
 library(data.table)
 
-tsa_numbers_df <- read.csv("visualizations/tsa_ordered.csv", header = TRUE, stringsAsFactors=FALSE)
+tsa_numbers_df <- read.csv("tsanumbers.csv", header = TRUE, stringsAsFactors=FALSE)
 tsa2 <- tsa_numbers_df[order(as.Date(tsa_numbers_df$Date, format="%m/%d", rev(tsa_numbers_df$X2021))),]
-travelstats <- read.csv("data/airtravel.csv",header = TRUE, stringsAsFactors=FALSE)
-travelstats <- travelstats %>% 
-  rename (Ontime.Prop= Ontime...., 
-          Delayed.Prop = Delayed...., 
-          Cancelled.Prop = Cancelled....) 
-travelstatistics <- read.csv("data/departstats_mod.csv",header = TRUE, stringsAsFactors=FALSE)
+travelstats <- read.csv("departstats_mod.csv",header = TRUE, stringsAsFactors=FALSE)
+
+travelstatistics <- read.csv("airtravel.csv",header = TRUE, stringsAsFactors=FALSE)
 dataset <- travelstatistics[-c(2, 4, 6,7), ]
-passengermod<- read.csv("data/ustraveltrends.csv", header = TRUE, stringsAsFactors=FALSE)
+passengermod<- read.csv("ustraveltrends.csv", header = TRUE, stringsAsFactors=FALSE)
 passenger_prop <- passengermod[-c(1,3,5),]
 passenger_num <- passengermod[-c(2,4,5),]
-covid_numbers_df <- read.csv("data/covid_cases.csv",header = TRUE, stringsAsFactors = FALSE)
+covid_numbers_df <- read.csv("covid_cases.csv",header = TRUE, stringsAsFactors = FALSE)
 covid_numbers_df <- covid_numbers_df%>%
   mutate(Dates = as.Date(Date,format = "%B %d %Y"))%>%
   select(Dates,X7.Day.Moving.Avg)
@@ -25,46 +22,46 @@ covid_numbers_df <- covid_numbers_df%>%
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
+  
   #line graph
   output$line <- renderPlotly({
-   if(input$tsa == "all") {
-     options(scipen=5)
-  ggplot(tsa2, aes(x= Date, group = 1)) + 
-      geom_line(aes(y = X2021, col="X2021")) + 
-      geom_line(aes(y= X2020, col="X2020")) + 
-      geom_line(aes(y= X2019, col = "X2019"))+
-      geom_line(aes(y= X2022, col = "X2022"), na.rm = TRUE) +
-      scale_x_discrete(limits=tsa2$Date)+
-      scale_colour_manual("", 
-                          breaks = c("X2021", "X2020", "X2019","X2022"),
-                          values = c("skyblue4", "black", "red", "darkolivegreen3"))+
-    ylab("Count")+ggtitle ("TSA checkpoint numbers")
-   } else if (input$tsa == "2021") {
-    ggplot(tsa2, aes(x= Date, y= X2021, group = 1)) + 
-       geom_line() + scale_x_discrete(limits=tsa2$Date) +
-       ylab("Count")+ggtitle ("TSA checkpoint numbers 2021")
-   }else if (input$tsa == "2020") {
-     ggplot(tsa2, aes(x= Date, y= X2020, group = 1)) + 
-       geom_line()+ scale_x_discrete(limits=tsa2$Date) +
-       ylab("Count")+ggtitle ("TSA checkpoint numbers 2020")
-   }  else if (input$tsa == "2019") {
-     ggplot(tsa2, aes(x= Date, y= X2019 , group = 1)) + 
-       geom_line()+ scale_x_discrete(limits=tsa2$Date)+
-       ylab("Count")+ggtitle ("TSA checkpoint numbers 2019")
-   } else {
-     ggplot(tsa2, aes(x= Date, y= X2022, group = 1)) + 
-       geom_line()+ scale_x_discrete(limits=tsa2$Date)+
-       ylab("Count")+ggtitle ("TSA checkpoint numbers 2022")
-   } 
-      })
+    if(input$tsa == "all") {
+      options(scipen=5)
+      ggplot(tsa2, aes(x= Date, group = 1)) + 
+        geom_line(aes(y = X2021, col="X2021")) + 
+        geom_line(aes(y= X2020, col="X2020")) + 
+        geom_line(aes(y= X2019, col = "X2019"))+
+        geom_line(aes(y= X2022, col = "X2022"), na.rm = TRUE) +
+        scale_x_discrete(limits=tsa2$Date)+
+        scale_colour_manual("", 
+                            breaks = c("X2021", "X2020", "X2019","X2022"),
+                            values = c("skyblue4", "black", "red", "darkolivegreen3"))+
+        ylab("Count")+ggtitle ("TSA checkpoint numbers")
+    } else if (input$tsa == "2021") {
+      ggplot(tsa2, aes(x= Date, y= X2021, group = 1)) + 
+        geom_line() + scale_x_discrete(limits=tsa2$Date) +
+        ylab("Count")+ggtitle ("TSA checkpoint numbers 2021")
+    }else if (input$tsa == "2020") {
+      ggplot(tsa2, aes(x= Date, y= X2020, group = 1)) + 
+        geom_line()+ scale_x_discrete(limits=tsa2$Date) +
+        ylab("Count")+ggtitle ("TSA checkpoint numbers 2020")
+    }  else if (input$tsa == "2019") {
+      ggplot(tsa2, aes(x= Date, y= X2019 , group = 1)) + 
+        geom_line()+ scale_x_discrete(limits=tsa2$Date)+
+        ylab("Count")+ggtitle ("TSA checkpoint numbers 2019")
+    } else {
+      ggplot(tsa2, aes(x= Date, y= X2022, group = 1)) + 
+        geom_line()+ scale_x_discrete(limits=tsa2$Date)+
+        ylab("Count")+ggtitle ("TSA checkpoint numbers 2022")
+    } 
+  })
   output$covid <- renderPlotly({
     attempt1 <- ggplot(covid_numbers_df, aes(x= Dates, y = X7.Day.Moving.Avg, group = 1)) + 
       geom_point(col="cadetblue2") + 
       geom_line(size = 0.25 , alpha = 0.5)+
       scale_x_date(date_labels = "%Y %b %d")
     attemptplot <- attempt1+ylab("Averages per week")+ggtitle ("Weekly Trends in Average Number of COVID-19")
- } )
+  } )
   
   
   #barchart
@@ -79,19 +76,19 @@ shinyServer(function(input, output) {
     }
     else if (input$choose == "2019") {
       plot_ly(passenger_num, x= ~X, y=~X2019, type = "bar") %>%
-       layout(title="2019 Trends", xaxis=list(title="Domestic vs International"), yaxis = list(title = 'Count'))
+        layout(title="2019 Trends", xaxis=list(title="Domestic vs International"), yaxis = list(title = 'Count'))
     } else if (input$choose == "2020"){
       plot_ly(passenger_num, x= ~X, y=~X2020, type = "bar") %>% 
         layout(title="2020 Trends", xaxis=list(title="Domestic vs International"), yaxis = list(title = 'Count'))
     } else if (input$choose == "2021") {
       plot_ly(passenger_num, x= ~X, y=~X2021, type = "bar") %>%
-      layout(title="2021 Trends", xaxis=list(title="Domestic vs International"), yaxis = list(title = 'Count'))
+        layout(title="2021 Trends", xaxis=list(title="Domestic vs International"), yaxis = list(title = 'Count'))
     }else if (input$choose == "2022"){
       plot_ly(passenger_num, x= ~X, y=~X2022, type = "bar") %>%
         layout(title="2022 Trends (as of May)", xaxis=list(title="Domestic vs International"), yaxis = list(title = 'Count'))
     }
   })
- 
+  
   #pie
   output$pie <- renderPlotly({
     if (input$select == "2019") {
@@ -111,7 +108,7 @@ shinyServer(function(input, output) {
               textinfo = "label+percent",
               insidetextorientation = "radial")  %>% layout(title = '2022 Pie Chart')
     }
-    })
+  })
   output$info <- renderTable({
     travelstats[7:10,]
   })
